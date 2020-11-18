6558b39e022b4fe739dff4d2ea09e28b21655272
majung2 <dlalwjd1417@likelion.org>
2020-11-18 16:06:32 +0900

    post31

---
+++ 26-JS/clip33.js
@@ -0,0 +1,26 @@
+class Animal {
+  constructor(type, name, sound) {
+    this.type = type;
+    this.name = name;
+    this.sound = sound;
+  }
+  say() {
+    console.log(this.sound);
+  }
+}
+class Dog extends Animal {
+  constructor(name, sound) {
+    super("개", name, sound);
+  }
+}
+class Cat extends Animal {
+  constructor(name, sound) {
+    super("고양이", name, sound);
+  }
+}
+const dog = new Dog("멍멍이", "멍멍");
+const cat = new Cat("야옹이", "야옹");
+const cat2 = new Cat("야오오오옹이", "야오오오오옹이");
+dog.say();
+cat.say();
+cat2.say();
