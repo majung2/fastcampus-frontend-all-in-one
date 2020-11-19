1e6d4c7e232955cb8963ab2f3895148b27428882
majung2 <dlalwjd1417@likelion.org>
2020-11-19 14:03:20 +0900

    post32

---
+++ 27-JS/clip01.js
@@ -0,0 +1,11 @@
+const array = [];
+let text =
+  array.length === 0 ? "배열이 비어있습니다." : "배열이 비어있지 않습니다.";
+console.log(text);
+
+const condition1 = false;
+const condition2 = false;
+
+const value = condition1 ? "와우" : condition2 ? "blabla" : "foo";
+
+console.log(value);
