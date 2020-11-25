ab5b6b8b5a6420848108ea6e3384913e1fd68a8e
majung2 <dlalwjd1417@likelion.org>
2020-11-25 18:02:36 +0900

    post38

--- 30-React/begin-react/src/App.js
+++ 30-React/begin-react/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
-import Counter from "./Counter";
+import InputSample from "./InputSample";
 
 function App() {
-  return <Counter />;
+  return <InputSample />;
 }
 
 export default App;
