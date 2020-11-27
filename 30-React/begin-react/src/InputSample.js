6ca7cc0582c274e8fa6aa5c44b5da661c1f7b729
majung2 <dlalwjd1417@likelion.org>
2020-11-27 22:19:38 +0900

    post40

--- 30-React/begin-react/src/InputSample.js
+++ 30-React/begin-react/src/InputSample.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function InputSample() {
   const [inputs, setInputs] = useState({
     name: "",
     nickname: "",
   });
+  const nameInput = useRef();
   const { name, nickname } = inputs;
   const onChange = (e) => {
     const { name, value } = e.target;
@@ -20,11 +21,18 @@ function InputSample() {
       name: "",
       nickname: "",
     });
+    nameInput.current.focus();
   };
 
   return (
     <div>
-      <input name="name" placeholder="이름" onChange={onChange} value={name} />
+      <input
+        name="name"
+        placeholder="이름"
+        onChange={onChange}
+        value={name}
+        ref={nameInput}
+      />
       <input
         name="nickname"
         placeholder="닉네임"
